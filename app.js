844540eddddf629cd81a6ebfd8df68f9b7fe75d0
oyut <oyutbileg@gmail.com>
2020-03-04 12:24:18 +0800

    Dice ajilagaand oruulsan

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //Toglogchiin eeljiig hadgalah huwisagch, negdugeer toglogch 0, 2 aar toglogchiig 1 gej temdegley
-var activePlayer = 1;
-//Toglogchdiin tsugluulsan onoog hadgalah huwisagch
+var activePlayer = 0;
+//Toglogchdiin niit tsugluulsan onoog hadgalah huwisagch
 var score = [0, 0];
 //Toglogchiin eeljindee tsugluulsan onoog hadgalah huwisagch
 var roundScore = 0;
@@ -16,9 +16,35 @@ document.getElementById("current-1").textContent = "0";
 
 var diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
-
+// Shoog shideh even listener
 document.querySelector(".btn-roll").addEventListener("click", function() {
+  //1 ees 6 hurtel sanamsargui neg toog gargaj avna
   var diceNumber = Math.floor(Math.random() * 6) + 1;
+  //shoonii zurgiig web deer gargaj irne
   diceDom.style.display = "block";
+  //buusan sanamsargui toond hargalzah shoonii zurgiig web deer gargaj irne
   diceDom.src = "dice-" + diceNumber + ".png";
+  //Toglogchiin eeljiin onoog uurchluh
+  if (diceNumber !== 1) {
+    roundScore = roundScore + diceNumber;
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
+  } else {
+    //1 buusan bol toglogchiin eeljiig solih
+    //Toglogchiin onoog 0 bolgon
+    roundScore = 0;
+    document.getElementById("current-" + activePlayer).textContent = 0;
+    //3 valsan operator ashiglay
+    //activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    if (activePlayer === 0) {
+      activePlayer = 1;
+    } else {
+      activePlayer = 0;
+    }
+    //Ulaan tsegiig shiluuleh
+
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+    //Shoog tur alga bolgoh
+    diceDom.style.display = "none";
+  }
 });
